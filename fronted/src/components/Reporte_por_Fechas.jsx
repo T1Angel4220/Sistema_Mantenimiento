da645742b96f2a463b2316db2af3d1540606524e
0AalL <alejosanchez456@gmail.com>
2025-01-15 17:02:51 -0500

    asdasdasd

--- fronted/src/components/Reporte_por_Fechas.jsx
+++ fronted/src/components/Reporte_por_Fechas.jsx
@@ -252,10 +252,10 @@ export default function MaintenanceReports() {
                 ],
             });
             const pieData = {
-                labels: ['Iniciados', 'Terminados', 'En Proceso'],
+                labels: ['Iniciados', 'Terminados'],
                 datasets: [
                     {
-                        data: [mantenimientos_iniciados, mantenimientos_terminados, mantenimientos_en_proceso],
+                        data: [mantenimientos_iniciados, mantenimientos_terminados],
                         backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
                         borderColor: ['rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
                         borderWidth: 1
